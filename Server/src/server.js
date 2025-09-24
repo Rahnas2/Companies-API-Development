aba509c3115d57a5d07bfba48ff08bd7099bf131
Rahnas p <rahnasx2@gmail.com>
2025-09-24 12:07:21 +0530

    fix: path missmatch issue 2

--- Server/src/server.js
+++ Server/src/server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use('/api/companies', companyRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.use(express.static(path.join(__dirname, '../../Client/build')));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../Client/build', 'index.html'));
   });
 }
 
