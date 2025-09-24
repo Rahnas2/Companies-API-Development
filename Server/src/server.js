3845af7ea733e510e5578f729e96c1d88a85ae16
Rahnas p <rahnasx2@gmail.com>
2025-09-24 11:57:33 +0530

    fix: path issue

--- Server/src/server.js
+++ Server/src/server.js
@@ -17,7 +17,7 @@ app.use('/api/companies', companyRoutes)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/build')));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
   });
 }
